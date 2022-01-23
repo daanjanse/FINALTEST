f554e2aaa1520c0b98b55cca3f9a9c58568fa08e
daanjanse <97192849+daanjanse@users.noreply.github.com>
2022-01-23 22:27:18 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -162,8 +162,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 8888) {
-      newMintAmount = 8888;
+    if (newMintAmount > 10) {
+      newMintAmount = 10;
     }
     setMintAmount(newMintAmount);
   };
